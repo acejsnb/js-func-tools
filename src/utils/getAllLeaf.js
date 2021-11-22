70b7000e91fcd99c4f83896afa802d51e4f61b2a
xiongshuang <xsmails@163.com>
2021-11-22 17:28:58 +0800

    add getFirstLeaf

--- src/utils/getAllLeaf.js
+++ src/utils/getAllLeaf.js
@@ -1,18 +1,27 @@
 /**
  * 获取所有叶子节点
  * @param tree
+ * @param first 是否获取第一个子节点的叶子节点
  * @returns {*[]}
  */
-const getAllLeaf = (tree) => {
+const getAllLeaf = (tree, first = false) => {
     const result = [];
-    const getLeaf = (tree) => {
-        tree.forEach((item) => {
-            if (!item.children) {
-                result.push(item);
+    const getLeaf = (data) => {
+        if (first) {
+            if (!data[0].children?.length) {
+                result.push(data[0]);
             } else {
-                getLeaf(item.children);
+                getLeaf(data[0].children, first);
             }
-        });
+        } else {
+            data.forEach((item) => {
+                if (!item.children?.length) {
+                    result.push(item);
+                } else {
+                    getLeaf(item.children);
+                }
+            });
+        }
     };
     getLeaf(tree);
     return result;
