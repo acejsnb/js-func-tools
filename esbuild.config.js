a48c4972c0cde4c50c06d57d66dba72ea02a0288
xiongshuang <xsmails@163.com>
2021-11-25 10:38:40 +0800

    add out banner

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,25 @@
 const esbuild = require('esbuild');
+const {name, version, author, license} = require('./package.json');
+const TimeFn = require('./get_time');
+
+// 输出文件添加注释
+const banner = `/**
+* @${name} v${version}
+* (c) 2019-2021 ${author}
+* Released under the ${license} License.
+* ${TimeFn()}
+*/`;
 
 esbuild.buildSync({
     entryPoints: ['src/index.js'],
+    // outdir: 'dist',
     outfile: 'dist/index.js',
     bundle: true,
     format: 'esm',
-    minify: true
+    minify: true,
     // target: ['es2020','chrome58','firefox57','safari11','edge16','node12']
     // target: ['chrome58']
+    banner: {
+        js: banner
+    }
 });
