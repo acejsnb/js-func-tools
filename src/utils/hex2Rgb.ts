6504fa5c4ee1d52bf6495ffefcbfc84d68670672
xiongshuang <xsmails@163.com>
2022-02-11 11:02:42 +0800

    change hex2Rgb

--- src/utils/hex2Rgb.ts
+++ src/utils/hex2Rgb.ts
@@ -5,8 +5,13 @@
 type Fn = (hex: string) => string;
 const hex2Rgb: Fn = (hex) => {
     const str = hex.substring(1);
-    let arr: number[] = str.split('').map(d => parseInt(d.padStart(2, '0'), 16));
-    if (str.length !== 3) arr = [parseInt(str.slice(0, 2), 16), parseInt(str.slice(2, 4), 16), parseInt(str.slice(4, 6), 16)];
+    let arr: number[] | string[];
+    if (str.length === 3) {
+        const [a, b, c] = str.split('');
+        arr = [parseInt(a.repeat(2), 16), parseInt(b.repeat(2), 16), parseInt(c.repeat(2), 16)];
+    } else {
+        arr = [parseInt(str.slice(0, 2), 16), parseInt(str.slice(2, 4), 16), parseInt(str.slice(4, 6), 16)];
+    }
     return `rgb(${arr.join(', ')})`;
 };
 
