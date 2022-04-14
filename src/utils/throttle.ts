97841501146bbbfa5b0c2b88504829ac5f25777a
xiongshuang <xsmails@163.com>
2022-04-14 14:09:19 +0800

    change throttle init status

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -6,14 +6,14 @@
 type Fn = (options?: {[key: string]: any}) => void
 type Throttle = (fn: Fn, delay?: number) => Fn
 const throttle: Throttle = (fn, delay = 500) => {
-    let timer: null | number, isFirst = true;
+    let timer: null | number, isFirst: boolean | null = true;
     return function (...args) {
         const context = this;
         const cb = () => {
             fn.apply(context, args);
             timer = null;
         };
-        isFirst && (isFirst = false, cb());
+        isFirst && (isFirst = null, timer = null, cb());
         !timer && (timer = setTimeout(cb, delay));
         return timer;
     };
