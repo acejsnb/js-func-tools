e7e788c266e8235952839e97dbdf1dd62f4077fa
xiongshuang <xsmails@163.com>
2022-05-23 17:35:56 +0800

    change scrollToTop.ts

--- src/utils/scrollToTop.ts
+++ src/utils/scrollToTop.ts
@@ -1,10 +1,11 @@
 // 平滑滚动到页面顶部
 type Fn = (id: string) => void
 const scrollToTop: Fn = (id) => {
-    const c = id ? ((document.getElementById(id) as HTMLElement)?.scrollTop ?? 0) : (document.documentElement.scrollTop || document.body.scrollTop);
+    const target = id ? (document.getElementById(id) as HTMLElement) : null;
+    const c = target ? (target?.scrollTop ?? 0) : (document.documentElement.scrollTop || document.body.scrollTop);
     if (c > 0) {
         window.requestAnimationFrame(() => scrollToTop(id));
-        window.scrollTo(0, c - c / 8);
+        (target ?? window).scrollTo(0, c - c / 8);
     }
 };
 
