1df94166e4cb429150f03e01174f63ce92268194
xsmails@163.com <ty-ekusa@mail.idcduty.com>
2022-08-20 11:29:08 +0800

    change obj2Url format

--- src/utils/obj2Url/index.ts
+++ src/utils/obj2Url/index.ts
@@ -8,13 +8,6 @@ import isNullObj from '../isNullObj';
  */
 type Obj = { [key: string]: any }
 type Obj2Url = (obj: Obj, url?: string) => string
-const obj2Url: Obj2Url = (obj, url = '') => {
-    if (isNullObj(obj)) return url;
-    const arr: string[] = [];
-    Object.keys(obj).forEach((key) => {
-        arr.push(`${key}=${encodeURIComponent(obj[key])}`);
-    });
-    return `${url}?${arr.join('&')}`;
-};
+const obj2Url: Obj2Url = (obj, url = '') => isNullObj(obj) ? url : `${url}?${new URLSearchParams(obj).toString()}`;
 
 export default obj2Url;
