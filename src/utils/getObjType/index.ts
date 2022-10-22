5f971c4e2b9a921f2d4d63268236022e6ff726f3
xsmails@163.com <ty-ekusa@mail.idcduty.com>
2022-10-22 18:12:08 +0800

    change getObjType

--- src/utils/getObjType/index.ts
+++ src/utils/getObjType/index.ts
@@ -1,10 +1,11 @@
 /**
  * 获取一个对象的类型
  * @param obj
+ * @param type
  */
 type Fn = (obj: any, type?: string) => string | boolean
 const getObjType: Fn = (obj, type = '') => {
-    const result = Object.prototype.toString.call(obj).substring(8).replace(']', '');
+    const result = Object.prototype.toString.call(obj).slice(8, -1);
     return type ? result === type : result;
 };
 
