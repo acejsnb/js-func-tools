ee9daafe8793e5f6c248516e75648669afbba69b
xs <xsmails@163.com>
2023-06-08 13:31:32 +0800

    feat: change randomColor

--- src/utils/randomColor/index.ts
+++ src/utils/randomColor/index.ts
@@ -1,4 +1,5 @@
 type Fn = () => string;
 // 生成随机16进制颜色
-const randomColor: Fn = () => '#' + Math.random().toString(16).slice(-6);
+// const randomColor: Fn = () => '#' + Math.random().toString(16).slice(-6);
+const randomColor: Fn = () => `#${(~~(Math.random()*(1<<24))).toString(16)}`;
 export default randomColor;
