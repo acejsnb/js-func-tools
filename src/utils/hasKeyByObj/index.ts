b1924aaeeeea39def69d9cdc31209f5db1488000
xs <xsmails@163.com>
2023-07-01 13:42:47 +0800

    feat: change hasKeyByObj

--- src/utils/hasKeyByObj/index.ts
+++ src/utils/hasKeyByObj/index.ts
@@ -4,7 +4,8 @@
  * @param key params
  */
 type Fn = (obj: any, key: string) => boolean
-const hasKeyByObj: Fn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+// const hasKeyByObj: Fn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+const hasKeyByObj: Fn = (obj, key) => key in obj;
 
 export default hasKeyByObj;
 
