a7ced45945bdbe17087e3d36cec359e1a252c44d
xs <xsmails@163.com>
2023-08-13 00:00:38 +0800

    feat: change round

--- src/utils/round/index.ts
+++ src/utils/round/index.ts
@@ -3,8 +3,8 @@
  * @param n 浮点数
  * @param dec 保留小数点的位数
  */
-declare const window: any;
-type Fn = (n: string | number, dec?: number) => number
-const round: Fn = (n=0, dec = 2) => window.Number(`${Math.round(Number(`${n}e${dec}`))}e-${dec}`);
+type Fn = (n: string | number, dec?: number) => number | string
+const round: Fn = (n=0, dec = 2) => Number(`${Math.round(Number(`${n}e${dec}`))}e-${dec}`);
+// const round: Fn = (n=0, dec = 2) => (Number(n) + Number.EPSILON).toFixed(dec);
 
 export default round;
