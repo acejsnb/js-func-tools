e874a99699f3c8cac03089cf5979fc43552b3d56
xs <xsmails@163.com>
2023-08-15 23:14:28 +0800

    fix: change domContainsTarget

--- src/utils/domContainsTarget/index.ts
+++ src/utils/domContainsTarget/index.ts
@@ -8,7 +8,7 @@ type Fn = (dom: HTMLElement, target: HTMLElement) => boolean;
 const domContainsTarget: Fn = (dom, target) => {
     if (dom === target) return true;
     if (dom?.contains) return dom?.contains(target);
-    if (dom?.compareDocumentPosition) return !!(dom?.compareDocumentPosition(target) & 16);
+    if (dom?.compareDocumentPosition) return (dom?.compareDocumentPosition(target) === 20);
     return false;
 };
 
