2aeb90279b3498e0d2f3a55609e3d2cfbaeb6da9
ace <xsmails@163.com>
2024-02-03 23:40:26 +0800

    feat: change removeHtmlTag

--- src/utils/removeHtmlTag/index.ts
+++ src/utils/removeHtmlTag/index.ts
@@ -1,4 +1,5 @@
 // 去除html标签
-const removeHtmlTag = (str: string): string => str.replace(/<[^>]+>/g,'');
+// const removeHtmlTag = (str: string): string => str.replace(/<[^>]+>/g,'');
+const removeHtmlTag = (str: string): string => new DOMParser().parseFromString(str, 'text/html').body.textContent || '';
 
 export default removeHtmlTag;
