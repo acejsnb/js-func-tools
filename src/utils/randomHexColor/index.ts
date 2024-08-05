d83f14640db08dabd51046c6a81f7541ec387a7e
ace <xsmails@163.com>
2024-08-05 22:49:54 +0800

    fix: randomHexColor

--- src/utils/randomHexColor/index.ts
+++ src/utils/randomHexColor/index.ts
@@ -1,5 +1,9 @@
 // 生成随机颜色
+import addZero from "../addZero";
 type RandomHexColor = () => void
-const randomHexColor: RandomHexColor = () => `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+const randomHexColor: RandomHexColor = () => {
+  const str = `${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+  return `#${addZero(str, 6)}`
+};
 
 export default randomHexColor;
